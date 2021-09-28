45e9cb8bc5d089003664b00e341e7d62cd78dfd7
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-28 18:27:30 +0300

    adds authentication on the signup view function

--- news/views.py
+++ news/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .signals import *
 from django.shortcuts import get_object_or_404, render,redirect, resolve_url
-from rest_framework.response import Response
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
@@ -15,6 +14,10 @@ from news.forms import *
 
 
 # Create your views here.
+def home(request):
+    posts = Neighborhood.objects.all().order_by('-pk')
+    return render(request, 'news/index.html',{'posts': posts})
+
 def signup(request):
     if request.method == 'POST':     
         form = SignUpForm(request.POST)
@@ -26,14 +29,10 @@ def signup(request):
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, f'Hi {username} Your account was created successfully. ')
-            return redirect('profile')
+            return redirect('home')
     else:
         form = SignUpForm
 
-    return render(request, 'registrations/signup.html', {'form':form,'registered': False } )
+    return render(request, 'registration/signup.html', {'form':form,'registered': False } )
 
 
-@login_required
-def home(request):
-    posts = Neighborhood.objects.all().order_by('-pk')
-    return render(request, 'news/index.html',{'posts': posts})
