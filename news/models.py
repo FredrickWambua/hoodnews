abed019e78618b0dffa68785ee456b599c7b8d47
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-28 14:44:39 +0300

    creates neighborhood model and its methods

--- news/models.py
+++ news/models.py
@@ -53,3 +53,53 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+class Neighborhood(models.Model):
+    name  = models.CharField(max_length=70)
+    admin = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='hood')
+    description = models.TextField(max_length=255)
+    location = models.CharField(max_length=70)
+    admin_contact = models.IntegerField(null=True, blank=True)
+    occupants = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='occupants')
+
+    def __str__(self) -> str:
+        return f'{self.name} occupants'
+
+    def create_neighborhood(self):
+        self.save()
+    
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def search_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=70, null=True)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=255)
+    location = models.CharField(max_length=55,null=True)
+    profile_photo = CloudinaryField('image')
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, related_name='occupants', blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        super().user()
+
+        img = Image.open(self.profile_photo.path)
+        if img.height >300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.profile_photo.path)
+
+    def delete_profile(self):
+        self.delete()
+
+
+
