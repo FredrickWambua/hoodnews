f3a7731c9b2819ff7d9bbef54f7983db1be63b47
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-28 18:26:06 +0300

    creates logina and logout urls

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
