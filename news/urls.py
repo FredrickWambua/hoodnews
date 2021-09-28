90fa709e690a242ab3871427b96d873cd0697b24
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-28 22:01:23 +0300

    configures url for profile

--- news/urls.py
+++ news/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('myprofile/', views.ProfileDetail, name='myprofile'),
+
+
 ]
