47aa48bfba7559d3a5b5e1b1f235091c045771c6
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-09-28 13:58:29 +0300

    creates a customuser model

--- news/models.py
+++ news/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from django.db import models
+from cloudinary.models import CloudinaryField
+from PIL import Image
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
@@ -32,3 +37,19 @@ class CustomUserManager(BaseUserManager):
         if kwargs.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True'))
         return self.create_user(email, password, **kwargs)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+
+    def __str__(self):
+        return self.email
